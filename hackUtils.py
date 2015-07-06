466f242059799f8ff8c7e2ace76ecc9140b9c290
avfisher <brianwangrewa@gmail.com>
2015-07-06 23:14:47 +0800

    Update hackUtils.py

--- hackUtils.py
+++ hackUtils.py
@@ -47,19 +47,23 @@ def isExisted(mystr,filepath):
         return False
 
 def getUrlRespHtml(url):
-    heads = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
-            'Accept-Charset':'GB2312,utf-8;q=0.7,*;q=0.7', 
-            'Accept-Language':'zh-cn,zh;q=0.5', 
-            'Cache-Control':'max-age=0', 
-            'Connection':'keep-alive', 
-            'Keep-Alive':'115',
-            'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.14) Gecko/20110221 Ubuntu/10.10 (maverick) Firefox/3.6.14'}
- 
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-    urllib2.install_opener(opener) 
-    req = urllib2.Request(url)
-    opener.addheaders = heads.items()
-    respHtml = opener.open(req).read()
+    respHtml=''
+    try:
+        heads = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+                'Accept-Charset':'GB2312,utf-8;q=0.7,*;q=0.7', 
+                'Accept-Language':'zh-cn,zh;q=0.5', 
+                'Cache-Control':'max-age=0', 
+                'Connection':'keep-alive', 
+                'Keep-Alive':'115',
+                'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.14) Gecko/20110221 Ubuntu/10.10 (maverick) Firefox/3.6.14'}
+     
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        urllib2.install_opener(opener) 
+        req = urllib2.Request(url)
+        opener.addheaders = heads.items()
+        respHtml = opener.open(req).read()
+    except Exception:
+        pass
     return respHtml
 
 def getLinksFromBaidu(html):  
